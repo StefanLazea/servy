30c1395e1cf0c7d2ae1cffdc5c016b530d391322
Stefan <lazeastefan@gmail.com>
2019-09-04 13:34:44 +0300

    First implementation for install script

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 #checks if python3 and venv are installed
 #if not, installs python3 and venv
+if command -v python3 &>/dev/null; then
+    echo Python 3 is installed
+else
+    echo Python 3 is now installing
+    sudo apt-get install python3
+fi
+
+
+if dpkg -s  python3-venv &> /dev/null; then
+    echo "Package python3-venv is installed!"
+else
+    echo installing venv
+    sudo apt-get install python3-venv  
+fi
+
 #creates the env directory
+python3 -m venv env --clear
+
 #activates the env
+source env/bin/activate
+
 #installs the dependencies from the "dependencies" file using pip
+pip3 install -r dependencies 
+pip freeze
+
 #creates an alias for history to current_directory/./launch.sh as a static path
 #prints a welcome message
 #gg wp
