bc62120b0ebed8d5ba52f5e465a335b05000eac4
Stefan <lazeastefan@gmail.com>
2019-09-13 17:27:55 +0300

    Remove old content in launch.sh

--- launch.sh
+++ launch.sh
@@ -7,34 +7,4 @@ cmd=$1;
 user=$(whoami)
 python py/command.py $EUID $user "$@"
 
-#deactivate
-#case $cmd in
-#    "init")
-#        python py/init.py
-#        ;;
-#    "write")
-#        python py/write.py $user "$@"
-#        ;;
-#    "read")
-#        python py/read.py $user "$@"
-#        ;;
-#    "delete")
-#        python py/delete.py $user $EUID "$@"
-#        ;;
-#    "share")
-#        python py/share.py "$@"
-#        ;;
-#    "change")
-#        python py/change.py $user "$@"
-#        ;;
-#    "version")
-#        python py/version.py
-#        ;;
-#    "help")
-#        python py/help.py
-#        ;;
-#    *)
-#        echo "servy - Command not known. Use 'servy help' for available commands!"
-#        ;;
-#esac
 deactivate
